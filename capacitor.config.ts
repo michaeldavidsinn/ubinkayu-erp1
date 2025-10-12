4d1f7da4e4cb01aa4f55c5f75437b3f9fa7b1966
michaeldavidsinn <146686816+michaeldavidsinn@users.noreply.github.com>
2025-10-12 12:48:03 +0700

    fix vercel error lago

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,20 +1,17 @@
 // file: capacitor.config.ts
 
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from '@capacitor/cli'
 
 const config: CapacitorConfig = {
   appId: 'com.ubinkayu.erp',
   appName: 'Ubinkayu ERP',
   webDir: 'out/renderer',
   server: {
-    // --- TAMBAHKAN BARIS DI BAWAH INI ---
     url: 'https://ubinkayu-erp1.vercel.app',
-    // ---------------------------------
+    hostname: 'ubinkayu-erp1.vercel.app', // <-- TAMBAHKAN BARIS INI
     androidScheme: 'https',
-    allowNavigation: [
-      'ubinkayu-erp1-git-erp1-mobile-cea6e7-michaeldavidsinns-projects.vercel.app'
-    ]
+    allowNavigation: ['ubinkayu-erp1.vercel.app']
   }
-};
+}
 
-export default config;
+export default config
